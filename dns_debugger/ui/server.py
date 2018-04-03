aa28bc6d7b1072bf896c651874101a9b31788ced
Eric Vergne <eric.vergne@corp.ovh.com>
2018-04-03 15:40:14 +0200

    Add monitoring route to server

--- dns_debugger/ui/server.py
+++ dns_debugger/ui/server.py
@@ -1,9 +1,15 @@
 """Create a small flask APP"""
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 
 from dns_debugger.executors import run_tests
 
 APP = Flask(__name__)
+
+
+@APP.route('/monitoring/ping')
+def ping():
+    """Check qname"""
+    return jsonify("pong")
 
 
 @APP.route('/<qname>')
